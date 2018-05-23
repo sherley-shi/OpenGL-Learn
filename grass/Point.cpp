f2b4e222c1f1420cecbdb066511d31ef3cb4b85a
vvyx <stevenshuang521@gmail.com>
2018-05-23 20:23:24 +0800

    Point Class

---
+++ grass/Point.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "./Point.h"
+
+
+Point::Point(double _x, double _y, double _dir){
+    this->x = _x;
+    this->y = _y;
+    this->dir = _dir;
+}
+
---
+++ grass/Point.h
@@ -0,0 +1,10 @@
+#ifndef  POINT_H
+#define POINT_H
+
+class Point{
+public:
+    double x, y, dir;
+    Point(double _x, double _y, double _dir);
+};
+
+#endif
